3237d4be18fdac24ed031623f1e1265c64f79b13
nandiniagg98 <42332120+nandiniagg98@users.noreply.github.com>
2018-09-19 15:06:23 +0530

    Add files via upload

---
+++ assignment 11/ass.java
@@ -0,0 +1,31 @@
+import java.io.*;
+class ass11
+{
+public static void main(String args[])
+{
+File f=new File("abc.txt");
+try
+{
+FileOutputStream fout=new FileOutputStream(f);
+String s="C:/Users/Test/video.mp4";
+char ch[]=s.toCharArray();
+for(int i=0;i<s.length;i++)
+{
+fout.write(ch[i]);
+}
+fout.close();
+FileInputStream fin=new FileInputStream(f);
+int i=fin.read();
+while(i!=-1)
+{
+System.out.print((char)i);
+i=fin.read();
+}
+fin.close();
+}
+catch(IOException e)
+{
+System.out.print(e);
+}
+}
+}
